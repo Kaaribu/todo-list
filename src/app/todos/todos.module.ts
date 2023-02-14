9ae8b1d859da751de59d8dbc17779d221a3fa5ea
Karabo Masalesa <kaaribu@protonmail.com>
2023-02-14 17:19:39 +0200

    Updated todos component names

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,7 +1,9 @@
 import {NgModule} from "@angular/core";
-
+import {TodosComponent} from "src/app/todos/components/todos/todos.component";
 @NgModule({
-  declarations: [],
+  declarations: [
+    TodosComponent
+  ],
   imports: [],
   providers: [],
   bootstrap: []
